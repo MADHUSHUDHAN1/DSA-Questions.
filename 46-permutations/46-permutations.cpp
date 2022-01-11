66662c40525a16cebec05df0cab532717691293f
MADHUSHUDHAN1 <57027124+MADHUSHUDHAN1@users.noreply.github.com>
2022-01-11 18:14:34 +0530

    Time: 3 ms (75.58%), Space: 8 MB (28.85%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        permuteRecursive(nums,0,res);
+        return res;
+    }
+    
+    void permuteRecursive(vector<int> nums,int i,vector<vector<int>>& res)
+    {
+        if(i == nums.size())
+        {
+            res.push_back(nums);
+        }
+        else {
+            for(int j=i; j<nums.size(); j++)
+            {
+                swap(nums[i],nums[j]);
+                permuteRecursive(nums,i+1,res);
+                swap(nums[i],nums[j]);
+            }
+        }
+    }
+    
+};
