6efee65fafe74aebd8c9257b20cb31209d404150
MADHUSHUDHAN1 <57027124+MADHUSHUDHAN1@users.noreply.github.com>
2022-04-21 22:23:06 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -9,88 +9,52 @@ using namespace std;
 // User function template for C++
 
 class Solution{
-    private:
-    bool isSafe(int x,int y,int n,vector<vector<int>> visited,vector<vector<int>> &m)
+   void solve(int i, int j, vector<vector<int>> &a, int n, vector<string> &ans, string move,
+    vector<vector<int>> &vis) 
     {
-        if((x>=0 && x<n) && (y>=0 && y<n) && visited[x][y] ==0 && m[x][y]==1)
+        if(i == n-1 && j == n-1)
         {
-            return true;
-        }
-        else
-        return false;
-    }
-    
-    void solve(vector<vector<int>> &m, int n,vector<string>& ans,int x,int y, vector<vector<int>> visited,string path)
-    {
-        if(x==n-1 && y==n-1)
-        {
-            ans.push_back(path);
+            ans.push_back(move);
             return;
         }
-        
-        visited[x][y]=1;
-        
-        //Down
-        int newx=x+1;
-        int newy=y;
-        if(isSafe(newx,newy,n,visited,m))
-        {
-            path.push_back('D');
-            solve(m,n,ans,newx,newy,visited,path);
-            path.pop_back();
-        }
-        
-         //Left
-        newx=x;
-        newy=y-1;
-        if(isSafe(newx,newy,n,visited,m))
-        {
-            path.push_back('L');
-            solve(m,n,ans,newx,newy,visited,path);
-            path.pop_back();
-        }
-        
-        //Right
-         newx=x;
-         newy=y+1;
-        if(isSafe(newx,newy,n,visited,m))
-        {
-            path.push_back('R');
-            solve(m,n,ans,newx,newy,visited,path);
-            path.pop_back();
-        }
-        
-         //Up
-        newx=x-1;
-        newy=y;
-        if(isSafe(newx,newy,n,visited,m))
-        {
-            path.push_back('U');
-            solve(m,n,ans,newx,newy,visited,path);
-            path.pop_back();
-        }
-        visited[x][y]=0;
+       // downward
+    if (i + 1 < n && !vis[i + 1][j] && a[i + 1][j] == 1) {
+      vis[i][j] = 1;
+      solve(i + 1, j, a, n, ans, move + 'D', vis);
+      vis[i][j] = 0;
+    }
+
+    // left
+    if (j - 1 >= 0 && !vis[i][j - 1] && a[i][j - 1] == 1) {
+      vis[i][j] = 1;
+      solve(i, j - 1, a, n, ans, move + 'L', vis);
+      vis[i][j] = 0;
+    }
+
+    // right 
+    if (j + 1 < n && !vis[i][j + 1] && a[i][j + 1] == 1) {
+      vis[i][j] = 1;
+      solve(i, j + 1, a, n, ans, move + 'R', vis);
+      vis[i][j] = 0;
+    }
+
+    // upward
+    if (i - 1 >= 0 && !vis[i - 1][j] && a[i - 1][j] == 1) {
+      vis[i][j] = 1;
+      solve(i - 1, j, a, n, ans, move + 'U', vis);
+      vis[i][j] = 0;
+    }
     }
     public:
     vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
         vector<string> ans;
-        if(m[0][0] == 0)
-        return ans;
-        
-        int r=0,c=0;
-        vector<vector<int>> visited = m;
-        for(int i=0; i<n; i++)
+        vector<vector<int>> visited(n,vector<int> (n,0));
+        if(m[0][0] == 1)
         {
-            for(int j=0; j<n; j++)
-            {
-                visited[i][j]=0;
-            }
+           // solve(0,0,m,n,visited,"",ans);
+            solve(0, 0, m, n, ans, "", visited);
         }
-        
-        string path="";
-        solve(m,n,ans,r,c,visited,path);
-        sort(ans.begin(),ans.end());
         return ans;
     }
 };
