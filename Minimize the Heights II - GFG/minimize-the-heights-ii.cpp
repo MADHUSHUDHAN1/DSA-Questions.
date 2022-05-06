9bcdf3078a3b76c65c7ce70c6d599890fbfa856b
MADHUSHUDHAN1 <57027124+MADHUSHUDHAN1@users.noreply.github.com>
2022-05-06 23:45:54 +0530

    Added solution - LeetHub

--- Minimize the Heights II - GFG/minimize-the-heights-ii.cpp
+++ Minimize the Heights II - GFG/minimize-the-heights-ii.cpp
@@ -12,20 +12,19 @@ class Solution {
     int getMinDiff(int arr[], int n, int k) {
         // code here
         sort(arr,arr+n);
-       int small;
-       int large,ans,mi,ma;
-       ans=arr[n-1]-arr[0];
-       small=arr[0]+k;
-       large=arr[n-1]-k;
-       
-       for(int i=0;i<n;i++)
-       {
-           mi=min(small,arr[i+1]-k);
-           ma=max(large,arr[i]+k);
-           if(mi<0) {continue;}
-           ans=min(ans,ma-mi);
-       }
-     return ans;
+        int small,large,mi,ma,ans;
+        ans = arr[n-1] - arr[0];
+        small = arr[0]+k;
+        large = arr[n-1]-k;
+        
+        for(int i=0; i<n; i++)
+        {
+            mi = min(small,arr[i+1]-k);
+            ma = max(large,arr[i]+k);
+            if(mi<0) continue;
+            ans = min(ans,ma-mi);
+        }
+        return ans;
     }
 };
 
