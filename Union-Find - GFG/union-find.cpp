964ef3ba517f04d0ab23e015b4df4ca95dfb40a5
MADHUSHUDHAN1 <57027124+MADHUSHUDHAN1@users.noreply.github.com>
2022-06-07 10:44:23 +0530

    Added solution - LeetHub

---
+++ Union-Find - GFG/union-find.cpp
@@ -0,0 +1,109 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    
+     int find(int x, int parent[])
+    {
+        if(parent[x] == x)
+        {
+            return x;
+        }
+        
+        parent[x] = find(parent[x], parent);
+        
+        return parent[x];
+    }
+    public:
+    //Function to merge two nodes a and b.
+    void union_( int x, int y, int parent[], int rank1[]) 
+    {
+        //code here
+        int x_rep = find(x, parent);
+        
+        int y_rep = find(y, parent);
+        
+        if(x_rep == y_rep)
+        {
+            return;
+        }
+        
+        if(rank1[x_rep] < rank1[y_rep])
+        {
+            parent[x_rep] = y_rep;
+        }
+        else if(rank1[x_rep] > rank1[y_rep])
+        {
+            parent[y_rep] = x_rep;
+        }
+        else
+        {
+            parent[y_rep] = x_rep;
+            
+            rank1[x_rep]++;
+        }
+        
+    }
+    
+    //Function to check whether 2 nodes are connected or not.
+    bool isConnected(int x,int y, int par[], int rank1[])
+    {
+        //code here
+        return find(x,par) == find(y,par);
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    //taking number of testcases
+    int t;
+    cin>>t;
+    while(t--) {
+        
+        //taking number of elements
+        int n;
+        cin>>n;
+        
+        
+        int par[n+1];
+        int rank1[n+1];
+
+        //initializing the parent and
+        //the rank array
+        for(int i=1; i<=n; i++) {
+            par[i] = i;
+            rank1[i] = 1;
+    }
+    
+    //taking number of queries
+    Solution obj;
+    int q;
+    cin>>q;
+    while(q--) {    
+        int a,b;
+        char c;
+        cin>>c>>a>>b;
+        
+        //if query type is 'U'
+        //then calling union_ method
+        if(c=='U')
+        {
+            obj.union_(a,b, par, rank1);
+        }
+        else//else calling isConnected() method
+        cout<<obj.isConnected(a,b, par, rank1)<<endl;
+    }
+    }
+    return 0;
+}
+  // } Driver Code Ends
