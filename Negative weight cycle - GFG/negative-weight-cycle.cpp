edfdcab99ea911ac4d3677c6da1b5d9c6d45a50a
MADHUSHUDHAN1 <57027124+MADHUSHUDHAN1@users.noreply.github.com>
2022-06-24 12:29:28 +0530

    Added solution - LeetHub

--- Negative weight cycle - GFG/negative-weight-cycle.cpp
+++ Negative weight cycle - GFG/negative-weight-cycle.cpp
@@ -10,7 +10,7 @@ public:
 	    
 	    vector<int> dist(n,INT_MAX);
 	    dist[0]=0;
-	    for(int i=0; i<n-1; i++)
+	    for(int i=1; i<n; i++)
 	    {
 	        for(auto it: edges) {
 	        if(dist[it[0]] != INT_MAX && dist[it[0]] + it[2] < dist[it[1]])
