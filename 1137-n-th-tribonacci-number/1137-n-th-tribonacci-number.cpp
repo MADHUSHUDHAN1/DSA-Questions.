d23ccc64350cf78e40eb8206abc3f8f408dc4f28
MADHUSHUDHAN1 <57027124+MADHUSHUDHAN1@users.noreply.github.com>
2022-06-30 22:46:03 +0530

    Time: 0 ms (100.00%), Space: 6 MB (30.38%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -3,14 +3,24 @@ public:
     int tribonacci(int n) {
         if(n<2)
             return n;
-        int dp[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] =1;
-        for(int i=3; i<=n;i++)
+        // int dp[n+1];
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // dp[2] =1;
+        int a=0,b=1,c=1;
+        int d = a+b+c;
+        // for(int i=3; i<=n;i++)
+        // {
+        //     dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        // }
+        while(n>2)
         {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            d = a+b+c;
+            a=b;
+            b=c;
+            c=d;
+            n--;
         }
-        return dp[n];
+        return c;
     }
 };
