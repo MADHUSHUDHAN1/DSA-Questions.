ebc663e244e65d9263298e74a0332ee905fbf492
MADHUSHUDHAN1 <57027124+MADHUSHUDHAN1@users.noreply.github.com>
2022-08-03 19:43:15 +0530

    Time: 8 ms (45.74%), Space: 12 MB (74.63%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size(),k,l;
-        for(k = n-2; k>=0; k--)
+        
+        int n=nums.size();
+        int k,l;
+        for(k=n-2; k>=0; k--)
         {
             if(nums[k]<nums[k+1])
+            {
                 break;
+            }
         }
-        
         if(k<0)
         {
             reverse(nums.begin(),nums.end());
@@ -16,11 +19,13 @@ public:
             for(l=n-1; l>k; l--)
             {
                 if(nums[l]>nums[k])
+                {
                     break;
+                }
             }
-            swap(nums[k],nums[l]);
-            reverse(nums.begin()+k+1,nums.end());
+             swap(nums[l],nums[k]);
+        reverse(nums.begin()+k+1,nums.end());
         }
-    
+       
     }
 };
