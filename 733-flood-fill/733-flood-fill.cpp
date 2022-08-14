8495ba60c0d403d46c9f2f2f36cba7adb8d123f5
MADHUSHUDHAN1 <57027124+MADHUSHUDHAN1@users.noreply.github.com>
2022-08-14 17:53:50 +0530

    Time: 12 ms (71.16%), Space: 14.3 MB (25.31%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,20 +1,50 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-       if(image[sr][sc] != newColor )
-           dfs(image,sr,sc,image[sr][sc],newColor);
-        return image;
-    }
-    
-    void dfs(vector<vector<int>> & image, int i,int j,int c0,int c1)
+    void f(vector<vector<int>>& image, int par, int n, int m, int sr, int sc, int color,vector<vector<int>> &visited)
     {
-        if(i<0 || j<0 || i>= image.size() || j>= image[0].size() || image[i][j] != c0)
-            return;
-        image[i][j]=c1;
-        dfs(image,i,j-1,c0,c1);
-        dfs(image,i,j+1,c0,c1);
-        dfs(image,i-1,j,c0,c1);
-        dfs(image,i+1,j,c0,c1);
         
+        //Up
+        if(sr-1>=0 && sr-1 <n && par == image[sr-1][sc] && !visited[sr-1][sc])
+        {
+            visited[sr][sc] = 1;
+            f(image,image[sr-1][sc],n,m,sr-1,sc,color,visited);
+        }
+        //Down
+        if(sr+1>=0 && sr+1 <n && par == image[sr+1][sc] && !visited[sr+1][sc])
+        {
+            visited[sr][sc] = 1;
+            f(image,image[sr+1][sc],n,m,sr+1,sc,color,visited);
+        }
+        //Left
+        if(sc-1>=0 && sc-1 <m && par == image[sr][sc-1] && !visited[sr][sc-1])
+        {
+            visited[sr][sc] = 1;
+            f(image,image[sr][sc-1],n,m,sr,sc-1,color,visited);
+        }
+        //Right
+        if(sc+1>=0 && sc+1 <m && par == image[sr][sc+1] && !visited[sr][sc+1])
+        {
+            visited[sr][sc] = 1;
+            f(image,image[sr][sc+1],n,m,sr,sc+1,color,visited);
+        }
+        visited[sr][sc] = 1;
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        
+        int n = image.size();
+        int m = image[0].size();
+        vector<vector<int>> visited(n,vector<int> (m,0));
+        f(image,image[sr][sc],n,m,sr,sc,color,visited);
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(visited[i][j] == 1)
+                {
+                    image[i][j] = color;
+                }
+            }
+        }
+        return image;
     }
 };
